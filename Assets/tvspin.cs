b7fd8cb0911d42eebd8fe7a2460110d07ad431f2
DESKTOP-SOQK8L7\mattb <jdm718@nyu.edu>
2021-10-15 17:59:18 -0400

    finished TV scene

---
+++ Assets/tvspin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tvspin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.A)))
+                    {
+                          transform.Rotate(Vector3.up, -10.0f * Time.deltaTime);
+                          
+                        
+    }
+     if ((Input.GetKey(KeyCode.D)))
+                    {
+                          transform.Rotate(Vector3.down, -10.0f * Time.deltaTime);
+                          
+                        
+    }
+}
+
+
+
+
+}
+
